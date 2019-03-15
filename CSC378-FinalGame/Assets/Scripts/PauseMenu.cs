5bf5bdb19fafe0b00e6251056c561e95288d7c66
Reynaldo Hermawan <reyhermawan97@gmail.com>
2019-03-15 15:03:31 -0700

    Stuff

---
+++ CSC378-FinalGame/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    /* Author: Connor French
+     * Description: functionality for the pause menu
+     */
+    public Button quit;
+    public AudioClip menuMusic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* Author: Connor French
+         * Description: checks if the player tries to pause the game when it is acceptable to pause the game
+         */
+        if (!GameControl.instance.gameOver && GameControl.instance.climbing)
+        {
+            if (GameControl.instance.paused)
+            {
+                quit.gameObject.SetActive(true);
+                quit.Select();
+            }
+            else
+            {
+                quit.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    public void returnToMenu()
+    {
+        /* Author: Connor French
+         * Description: returns to the main menu from the pause menu
+         */
+        GameControl.instance.speedyMusic = false;
+        Music.instance.music.clip = menuMusic;
+        Music.instance.music.volume = 1;
+        Music.instance.music.pitch = 1;
+        Music.instance.music.Play();
+        SceneManager.LoadScene("Main_Menu");
+    }
+
+    public void playAgain()
+    {
+        /* Author: Connor French
+         * Description: reloads current scene from beginning
+         */
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
