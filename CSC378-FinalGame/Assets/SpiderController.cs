2e563573cdfabbaa9c2dfc7324b5f9b19b52f9bc
Reynaldo Hermawan <reyhermawan97@gmail.com>
2019-03-15 12:10:31 -0700

    Spider changes

--- CSC378-FinalGame/Assets/SpiderController.cs
+++ CSC378-FinalGame/Assets/SpiderController.cs
@@ -9,6 +9,8 @@ public class SpiderController : MonoBehaviour
     public GameObject spiderShotPrefab;
     public float delayAttackTime;
 
+    public GameObject fireLocation;
+
     private float delayTime = 0;
     // Start is called before the first frame update
     void Start()
@@ -22,7 +24,7 @@ public class SpiderController : MonoBehaviour
         RaycastHit2D hit = Physics2D.Raycast(transform.position, player.transform.position - transform.position, attackRange);
         Debug.DrawRay(transform.position, (player.transform.position - transform.position).normalized*attackRange , Color.red);
         if(hit.collider != null && hit.rigidbody.gameObject == player && delayTime <= 0){
-            GameObject shot = Instantiate(spiderShotPrefab, new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z), this.transform.rotation);
+            GameObject shot = Instantiate(spiderShotPrefab, new Vector3(fireLocation.transform.position.x, fireLocation.transform.position.y, fireLocation.transform.position.z), fireLocation.transform.rotation);
             delayTime = delayAttackTime;
         }
 
